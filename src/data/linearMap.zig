a209adf11ca02cfe298f5ef0af56abdf47fa517c
ProfessorLagg <futurecake2999@gmail.com>
2025-03-01 13:17:16 +0100

    begun writing linear map

---
+++ src/data/linearMap.zig
@@ -0,0 +1,43 @@
+const builtin = @import("builtin");
+const std = @import("std");
+
+pub fn EqualFn(comptime T: type) type {
+    return (fn (T, T) bool);
+}
+pub fn LinearMap(comptime TKey: type, comptime TVal: type, comptime equals: EqualFn(TKey)) type {
+    return struct {
+        const TSelf = @This();
+
+        allocator: std.mem.Allocator,
+        keybuf: []TKey,
+        valbuf: []TVal,
+
+        keys: []TKey,
+        vals: []TVal,
+
+        pub fn init(allocator: std.mem.Allocator) !TSelf {
+            var r = TSelf{
+                .allocator = allocator,
+                .keybuf = try allocator.alloc(TKey, 2),
+                .valbuf = try allocator.alloc(TVal, 2),
+            };
+
+            r.keys = r.keybuf[0..0];
+            r.val = r.valbuf[0..0];
+        }
+
+        pub fn deinit(self: *TSelf) void {
+            self.allocator.free(self.keybuf);
+            self.allocator.free(self.valbuf);
+        }
+
+        pub fn put(self: *TSelf, key: TKey, val: TVal) !void {
+            if (self.keys.len == self.keybuf.len) {
+                // TODO Resize
+            }
+
+            _ = &key;
+            _ = &val;
+        }
+    };
+}

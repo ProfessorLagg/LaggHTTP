7ece1e14cc428de2573c490fd3cf3b6b94aa8f72
ProfessorLagg <futurecake2999@gmail.com>
2025-05-23 19:18:37 +0200

    request parsing done

--- src/httpContext.zig
+++ src/httpContext.zig
@@ -31,7 +31,7 @@ pub const HttpContext = struct {
     response: HttpResponse,
 };
 
-pub const HttpHeader = struct {
+pub const HttpHeaderField = struct {
     key: []const u8,
     val: []const u8,
 };
@@ -50,93 +50,103 @@ pub fn HttpRequest(comptime settings: HttpRequestSettings) type {
 
         /// Contains both request line and HTTP header fields
         header: []const u8,
-
-
+        requestLine: []const u8,
         method: []const u8,
         path: []const u8,
         version: []const u8,
-        rawHeaders: []const u8,
+        rawFields: []const u8,
         body: []const u8,
 
-        fn find_header(rawHeaders: []const u8, header_key: []const u8) ?HttpHeader {
-            const start: usize = utils.Strings.indexOf(rawHeaders, header_key) orelse return null;
-            var slice = rawHeaders[start..];
+        fn find_field(fields: []const u8, header_key: []const u8) ?HttpHeaderField {
+            const start: usize = utils.Strings.indexOf(fields, header_key) orelse return null;
+            var slice = fields[start..];
             const end: usize = utils.Strings.indexOf(slice, "\r\n") orelse slice.len;
             slice = slice[0..end];
-            const splitIndex: usize = utils.indexOf(slice, ": ") orelse return null;
-            return HttpHeader{
+            const splitIndex: usize = utils.Strings.indexOf(slice, ": ") orelse return null;
+            return HttpHeaderField{
                 .key = slice[0..splitIndex],
                 .val = slice[splitIndex + 2 ..],
             };
         }
-        pub fn init_v0(reader: std.io.AnyReader, allocator: std.mem.Allocator) !HttpRequest(settings) {
-            var stack_buffer: [settings.max_requestLine_size + settings.max_headers_size]u8 = undefined;
-            @memset(stack_buffer[0..], 0);
-            var i = 0;
-
-            var method: []const u8 = stack_buffer[i..];
-            while (stack_buffer[i] != ' ') {
-                i[i] = try reader.readByte();
-                i += 1;
+        fn parse_request_line(self: *HttpRequest(settings)) !void {
+            var iter = std.mem.splitScalar(u8, self.requestLine, ' ');
+            self.method = iter.next() orelse return HttpRequestError.MalformedRequestLine;
+            self.path = iter.next() orelse return HttpRequestError.MalformedRequestLine;
+            self.version = iter.next() orelse return HttpRequestError.MalformedRequestLine;
+        }
+        pub fn init(reader: anytype, allocator: std.mem.Allocator) !HttpRequest(settings) {
+            var header_buffer: [settings.max_requestLine_size + settings.max_headers_size]u8 = undefined;
+            @memset(header_buffer[0..], 0);
+            var i: usize = 2;
+            header_buffer[0] = reader.readByte() catch return HttpRequestError.MalformedRequestLine;
+            header_buffer[1] = reader.readByte() catch return HttpRequestError.MalformedRequestLine;
+            while (!std.mem.eql(u8, header_buffer[i - 2 .. i], "\r\n")) : (i += 1) {
                 if (i >= settings.max_requestLine_size) return HttpRequestError.RequestLineTooLong;
+                header_buffer[i] = reader.readByte() catch return HttpRequestError.MalformedRequestLine;
             }
-            method.len = i;
-            i += 1;
-
-            var path: []const u8 = stack_buffer[i..];
-            while (stack_buffer[i] != ' ') {
-                stack_buffer[i] = try reader.readByte();
-                i += 1;
-                if (i >= settings.max_requestLine_size) return HttpRequestError.RequestLineTooLong;
-            }
-            path.len = i;
-            i += 1;
-
-            var version: []const u8 = stack_buffer[i..];
-            while (stack_buffer[i - 1] != '\r' and stack_buffer[i] != '\n') {
-                stack_buffer[i] = try reader.readByte();
-                i += 1;
-                if (i >= settings.max_requestLine_size) return HttpRequestError.RequestLineTooLong;
-            }
-            version.len = i - 2;
-            i += 1;
-
-            // Headers Segment
-            const headers_start: usize = i;
-            var rawHeaders: []const u8 = stack_buffer[i..];
-
-            while (stack_buffer[i - 3] != '\r' and stack_buffer[i - 2] != '\n' and stack_buffer[i - 1] != '\r' and stack_buffer[i] != '\n') {
-                stack_buffer[i] = try reader.readByte();
-                i += 1;
-                if ((i - headers_start) >= settings.max_headers_size) return HttpRequestError.HeaderSegmentTooLong;
-            }
-            rawHeaders.len = i - 4;
-            i += 1;
-            const stack_bytes: []const u8 = stack_buffer[0..i];
-            // Body
-            var body_buffer: []const u8 = [0]u8;
-            if (find_header(rawHeaders, "Content-Length")) |content_length_header| {
-                // Request had Content-Length header
-                const content_length_value: usize = utils.Strings.fastIntParse(content_length_header.val);
-                if (content_length_value >= 0) {
-                    if (content_length_value >= settings.max_body_size) return HttpRequestError.BodyTooLong;
-                    body_buffer = try allocator.alloc(u8, content_length_value);
-                    body_buffer.len = reader.read(body_buffer);
-                }
+            const fields_start_i = i;
+            while (!std.mem.eql(u8, header_buffer[i - 4 .. i], "\r\n\r\n")) : (i += 1) {
+                if ((i - fields_start_i) >= settings.max_headers_size) return HttpRequestError.RequestLineTooLong;
+                header_buffer[i] = reader.readByte() catch return HttpRequestError.MalformedRequestLine;
             }
 
-            const request_buffer_len: usize = stack_bytes + body_buffer.len;
-            const result_buffer: []const u8 = try allocator.alloc(u8, request_buffer_len);
+            var header: []u8 = try allocator.alloc(u8, i);
+            std.mem.copyForwards(u8, header, header_buffer[0..i]);
+
             var result: HttpRequest(settings) = .{
-                .buffer = result_buffer[0..],
-                .method = result_buffer[method_start..method_end],
-                .path = result_buffer[path_start..path_end],
-                .version = result_buffer[version_start..version_end],
-                .rawHeaders = result_buffer[version_start..version_end],
+                .header = header,
+                .requestLine = header[0 .. fields_start_i - 2],
+                .rawFields = header[fields_start_i..],
+
+                .method = header[0..0],
+                .path = header[0..0],
+                .version = header[0..0],
                 .body = undefined,
             };
+            try result.parse_request_line();
+
+            if (find_field(result.rawFields, "Content-Length")) |field| {
+                const content_length_value: usize = @intCast(utils.Strings.fastIntParse(isize, field.val));
+                if (content_length_value > settings.max_body_size) return HttpRequestError.BodyTooLong;
+                var body: []u8 = try allocator.alloc(u8, content_length_value);
+                const body_size: usize = try reader.read(body);
+                if (body.len != body_size) {
+                    const temp_body: []u8 = try allocator.alloc(u8, body_size);
+                    std.mem.copyForwards(u8, temp_body, body);
+                    allocator.free(body);
+                    body = temp_body;
+                }
+                result.body = body[0..];
+            }
+
+            _ = &result;
+            return result;
+        }
+
+        pub fn deinit(self: *HttpRequest(settings), allocator: std.mem.Allocator) void {
+            allocator.free(self.header);
+            allocator.free(self.body);
+        }
+
+        test init {
+            const allocator = std.testing.allocator;
+            
+            const http_post = @embedFile("testdata/post.txt");
+            var stream = std.io.fixedBufferStream(http_post);
+            const reader = stream.reader();
+            var request: HttpRequest(settings) = try HttpRequest(settings).init(reader, allocator);
+            defer request.deinit(allocator);
+
+            try std.testing.expectEqualStrings("POST", request.method);
+            try std.testing.expectEqualStrings("/index.html", request.path);
+            try std.testing.expectEqualStrings("HTTP/1.1", request.version);
+            try std.testing.expectEqualStrings("DATADATADATADATADATADATADATADATA", request.body);
         }
     };
 }
 
 pub const HttpResponse = struct {};
+
+test "HttpRequest" {
+    _ = HttpRequest(.{});
+}
--- src/root.zig
+++ src/root.zig
@@ -5,8 +5,3 @@ usingnamespace httpContextNs;
 test {
     _ = httpContextNs;
 }
-
-test "temp test" {
-    var body_buffer: []const u8 = [0]u8;
-    @memset(body_buffer[0..], 0);
-}

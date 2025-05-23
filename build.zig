cdad916c2535db7cf1e8ff82f58566afe9bd97d7
ProfessorLagg <futurecake2999@gmail.com>
2025-05-23 14:09:39 +0200

    init rewrite

--- build.zig
+++ build.zig
@@ -1,60 +1,29 @@
 const std = @import("std");
-const Build = std.Build;
-const Step = Build.Step;
 
-const FilePaths = struct {
-    pub const rootPath = "src/root.zig";
-    pub const debuggerPath = "src/debugger.zig";
-
-    pub const testPaths = [_][]const u8{ // NO FOLD
-        rootPath,
-        "src/http/httpServer.zig",
-        "src/utils.zig",
-    };
-
-    pub fn addTestStep(b: *Build, target: Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) *Step {
-        const test_step: *std.Build.Step = b.step("test", "Run unit tests");
-        for (testPaths) |path| {
-            const unit_tests = b.addTest(.{
-                .root_source_file = b.path(path),
-                .target = target,
-                .optimize = optimize,
-            });
-            const run_unit_tests = b.addRunArtifact(unit_tests);
-            test_step.dependOn(&run_unit_tests.step);
-        }
-        return test_step;
-    }
-};
-
-pub fn build(b: *Build) void {
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "LaggHTTP",
-        .root_source_file = b.path(FilePaths.rootPath),
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
+    });
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "LaggHTTP",
+        .root_module = lib_mod,
     });
     b.installArtifact(lib);
 
-    const debugger_exe = b.addExecutable(.{
-        .name = "debugger",
-        .root_source_file = b.path(FilePaths.debuggerPath),
-        .target = target,
-        .optimize = optimize,
-        .single_threaded = true,
-        .link_libc = false,
+
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
     });
-    b.installArtifact(debugger_exe);
-
-    const run_debugger = b.addRunArtifact(debugger_exe);
-    run_debugger.step.dependOn(b.getInstallStep());
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_debugger.step);
-
-    const test_step = FilePaths.addTestStep(b, target, optimize);
-    _ = &test_step;
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,2 @@
 const std = @import("std");
-const httpServer = @import("httpServer.zig");
-const httpRequest = @import("httpRequest.zig");
-const httpResponse = @import("httpResponse.zig");
 
-pub usingnamespace httpServer;
-pub usingnamespace httpRequest;
-pub usingnamespace httpResponse;

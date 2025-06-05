1dcae9b1e43903526434fa1bbc569325cf7f70c7
ProfessorLagg <futurecake2999@gmail.com>
2025-06-05 16:20:16 +0200

    the _debugger TCP thing is FINALLY fast
    Turns out it mightve been winrk

--- src/_debugger.zig
+++ src/_debugger.zig
@@ -17,12 +17,14 @@ pub const std_options: std.Options = .{
 };
 
 pub fn main() !void {
-    // try debugStandardHttpServer();
+    try debugStandardHttpServer();
     // try debugTCPListener();
-    try debug_ws2_32();
+    // try debug_ws2_32();
 }
 
-fn debug_ws2_32() !void {
+fn debug_ws2_32_old() !void {
+    //https://heyulong3d.medium.com/network-programming-simple-hello-world-tcp-in-c-c-on-windows-and-unix-539d5f47733e
+
     const utils = LaggHTTP.utils;
     const ws2_32 = std.os.windows.ws2_32;
     var wsaData: ws2_32.WSADATA = undefined;
@@ -79,6 +81,96 @@ fn debug_ws2_32() !void {
         const send_result = ws2_32.send(clnt_sock, rsp.ptr, @intCast(rsp.len), @as(i32, 0));
         const send_len: usize = @intCast(send_result);
         std.log.debug("sent resposne:\n\"{s}\"", .{rsp[0..send_len]});
+    }
+}
+
+fn debug_ws2_32() !void {
+    //https://learn.microsoft.com/en-us/windows/win32/winsock/complete-server-code
+
+    const utils = LaggHTTP.utils;
+    const ws2_32 = std.os.windows.ws2_32;
+
+    var iResult: i32 = 0;
+
+    var wsaData: ws2_32.WSADATA = undefined;
+    const WSAStartupResult = ws2_32.WSAStartup(0x202, &wsaData);
+    if (WSAStartupResult != 0) {
+        ws2_32.WSASetLastError(WSAStartupResult);
+        return utils.meta.EnumValueToError(ws2_32.WinsockError, ws2_32.WSAGetLastError());
+    }
+    defer _ = ws2_32.WSACleanup();
+    std.log.debug("WSAStartup() success!", .{});
+
+    // var result: ws2_32.addrinfo = undefined;
+    var result: ?*ws2_32.addrinfo = null;
+
+    var hints: ws2_32.addrinfo = std.mem.zeroes(ws2_32.addrinfo);
+    hints.family = ws2_32.AF.INET;
+    hints.socktype = ws2_32.SOCK.STREAM;
+    hints.protocol = ws2_32.IPPROTO.TCP;
+    hints.flags = ws2_32.AI{};
+
+    // Resolve the server address and port
+    iResult = ws2_32.getaddrinfo("127.0.0.1", "5500", &hints, &result);
+    if (iResult != 0) {
+        ws2_32.WSASetLastError(iResult);
+        return utils.meta.EnumValueToError(ws2_32.WinsockError, ws2_32.WSAGetLastError());
+    }
+    std.log.debug("getaddrinfo() succes!: {any}", .{result.?.*});
+
+    const ListenSocket = ws2_32.socket(result.?.family, result.?.socktype, result.?.protocol);
+    if (ListenSocket == ws2_32.INVALID_SOCKET) return utils.meta.EnumValueToError(ws2_32.WinsockError, ws2_32.WSAGetLastError());
+    defer _ = ws2_32.closesocket(ListenSocket);
+    std.log.debug("socket() success!", .{});
+
+    iResult = ws2_32.bind(ListenSocket, result.?.addr.?, @intCast(result.?.addrlen));
+    if (iResult == ws2_32.SOCKET_ERROR) return utils.meta.EnumValueToError(ws2_32.WinsockError, ws2_32.WSAGetLastError());
+    std.log.debug("socket() success!", .{});
+
+    ws2_32.freeaddrinfo(result);
+
+    const listen_result = ws2_32.listen(ListenSocket, ws2_32.SOMAXCONN);
+    if (listen_result == ws2_32.SOCKET_ERROR) return utils.meta.EnumValueToError(ws2_32.WinsockError, ws2_32.WSAGetLastError());
+    std.log.debug("listen() success!", .{});
+
+    var read_arr: [65_536]u8 = undefined;
+    var read_buf: []u8 = read_arr[0..];
+
+    const rsp_arr = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+    var rsp: []const u8 = rsp_arr[0..];
+    while (true) {
+        read_buf = read_arr[0..];
+        @memset(read_buf, 0);
+        const ClientSocket = ws2_32.accept(ListenSocket, null, null);
+        defer {
+            _ = ws2_32.shutdown(ClientSocket, ws2_32.SD_BOTH);
+            _ = ws2_32.closesocket(ClientSocket);
+        }
+        std.log.debug("accept() success!", .{});
+
+        if (ClientSocket == ws2_32.INVALID_SOCKET) {
+            const err = utils.meta.EnumValueToError(ws2_32.WinsockError, ws2_32.WSAGetLastError());
+            std.log.err("{any}{any}", .{ err, @errorReturnTrace() });
+            continue;
+        }
+
+        const recv_result = ws2_32.recv(ClientSocket, read_buf.ptr, @intCast(read_arr.len), @as(i32, 0));
+        if (recv_result == ws2_32.SOCKET_ERROR) {
+            const err = utils.meta.EnumValueToError(ws2_32.WinsockError, ws2_32.WSAGetLastError());
+            std.log.err("recv() failed: {any}{any}", .{ err, @errorReturnTrace() });
+            continue;
+        }
+        read_buf.len = @intCast(recv_result);
+        std.log.debug("recv() sucess!:\n\"{s}\"", .{read_buf});
+
+        const send_result = ws2_32.send(ClientSocket, rsp.ptr, @intCast(rsp.len), @as(i32, 0));
+        if (send_result == ws2_32.SOCKET_ERROR) {
+            const err = utils.meta.EnumValueToError(ws2_32.WinsockError, ws2_32.WSAGetLastError());
+            std.log.err("send() failed: {any}{any}", .{ err, @errorReturnTrace() });
+            continue;
+        }
+        const send_len: usize = @intCast(send_result);
+        std.log.debug("send() sucess!:\n\"{s}\"", .{rsp[0..send_len]});
     }
 }
 
